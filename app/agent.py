bfb0a7703e51f0f750a51d31210ae8b3469749ad
srinivas rachamadugu <srinivasrachamadugu@srinivass-MacBook-Air.local>
2025-07-14 20:50:56 -0400

    fixed streamlit errors

--- app/agent.py
+++ app/agent.py
@@ -3,11 +3,13 @@ from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import RetrievalQA
+from langchain.prompts import PromptTemplate
+
 
 from resume_loader import load_resume
 
 class ResumeAgent:
-    def __init__(self, resume_path: str = "data/resume.pdf", prompt_path: str = "app/prompt_template.txt"):
+    def __init__(self, resume_path: str = "data/My_Resume_Nikhil_Racha_K.pdf", prompt_path: str = "app/prompt_template.txt"):
         self.db = self._build_vector_store(resume_path)
         self.chain = self._build_chain(prompt_path)
 
@@ -19,13 +21,20 @@ class ResumeAgent:
     def _build_chain(self, prompt_path: str):
         llm = ChatOpenAI(temperature=0)
         retriever = self.db.as_retriever()
+
         with open(prompt_path, 'r') as f:
             system_prompt = f.read()
+
+        prompt = PromptTemplate(
+            input_variables=["context", "question"],  # Most RetrievalQA chains use these
+            template=system_prompt
+        )
+
         return RetrievalQA.from_chain_type(
             llm=llm,
             chain_type="stuff",
             retriever=retriever,
-            chain_type_kwargs={"prompt": system_prompt},
+            chain_type_kwargs={"prompt": prompt},
         )
 
     def ask(self, question: str) -> str:
